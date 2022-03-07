a3f61ba717b5732f06936bec18bea0fd62c46aa7
bhargavabhamidipati <bhargavabhamidipati@gmail.com>
2022-03-07 11:26:31 +0100

    adding calculator

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+    public int add(int a, int b){
+        return a+b;
+    }
+    public int substract(int a, int b){
+        return a-b;
+    }
+}
